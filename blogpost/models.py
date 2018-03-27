9b82e43de3d6e2bf341ecfcb5e6a1b78a36b6bc4
qinglian <diqinglian@gmail.com>
2018-03-28 00:31:09 +0800

    set blogpost.

--- blogpost/models.py
+++ blogpost/models.py
@@ -3,9 +3,9 @@ from django.db.models import permalink
 
 # Create your models here.
 class Blogpost(models.Model):
-    title = models.CharFied(max_length=100, unique=True)
-    author = models.CharFied(max_length=100, unique=True)
-    slug = models.CharFied(max_length=100, unique=True)
+    title = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
 
